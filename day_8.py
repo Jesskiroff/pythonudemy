66cf6b1cdb0c7a5ece21b1c4ae331f7dc015b993
Jessica Kiroff <jesskiroff@gmail.com>
2024-07-04 14:41:00 -0400

    create day 8 file

---
+++ day_8.py
@@ -0,0 +1,17 @@
+#funcs
+# parameter vs argument
+#def my_function():
+    #Do this
+    #y=then do this
+    # finally do this
+
+#my_function()
+
+
+name = input("What is your name? ")
+def jessica():
+    print ("Hello, ")
+    print ("My name is ")
+    print (name)
+
+jessica()
