0b40df7fd78df416a702e6cff2724017ec9fe165
Jessica Kiroff <jesskiroff@gmail.com>
2024-07-04 15:44:21 -0400

    add funcs with multiple parameters

--- day_8.py
+++ day_8.py
@@ -8,10 +8,27 @@
 #my_function()
 
 
-name = input("What is your name? ")
-def jessica():
-    print ("Hello, ")
-    print ("My name is ")
-    print (name)
+# name = input("What is your name? ")
+# def jessica():
+#     print ("Hello, ")
+#     print ("My name is ")
+#     print (name)
 
-jessica()
+# jessica()
+
+
+
+#name is parameter
+#Jess is the argument
+# def greet(name):
+#     print(f"Hello, {name}")
+#     print(f"How are you today, {name}?")
+
+# greet("Jess")
+
+#func w 2 parameters
+def greet_with(name, location):
+    print(f"Hello, {name}")
+    print(f"WHat is it like in {location} ?")
+
+greet_with("Jess", "nomansland")
